b7f814bc8650a6c96866563885693d3d20b7584b
RodartHub <rodrigovalencia95@hotmail.com>
2023-07-23 00:16:35 -0500

    v4

--- api_rev/settings.py
+++ api_rev/settings.py
@@ -158,5 +158,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-CORS_ALLOWED_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ['*']
 CORS_ALLOW_CREDENTIALS = True
